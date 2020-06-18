77b91442a2f48ecd0d9d5e3e24a2c4ab1c10da83
masayukinii1011 <masayukinii1011@gmail.com>
2020-06-18 16:32:19 +0900

    データ取得クエリ作成

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,30 +1,44 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import { Card, CardBody, Table } from 'reactstrap';
+import { MOVIE_LIST } from '../query/query'
 
 
 function MovieList() {
-  return (
-    <Card>
-      <CardBody>
-        <Table>
-          <thead>
-            <tr>
-              <th>タイトル</th>
-              <th>ジャンル</th>
-              <th>監督</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>タイトル</td>
-              <td>ジャンル</td>
-              <td>監督</td>
-            </tr>
-          </tbody>
-        </Table>
-      </CardBody>
-    </Card>
-  );
+  const { loading, error, data } = useQuery(MOVIE_LIST)
+  if (loading) {
+    return <p>Loading...</p>
+  } else if (error) {
+    return <p>Error</p>
+  } else {
+    return (
+      <Card>
+        <CardBody>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>タイトル</th>
+                <th>ジャンル</th>
+                <th>監督</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.movies.map(({ id, name, genre, director }) => (
+                  <tr key={id}>
+                    <td>{name}</td>
+                    <td>{genre}</td>
+                    <td>{director}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </Table>
+        </CardBody>
+      </Card>
+    );
+  }
 }
 
 export default MovieList;
