4aceb527ac42d69b15dba654422beea739405eea
masayukinii1011 <masayukinii1011@gmail.com>
2020-06-18 20:07:07 +0900

    refetch機能

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { useForm } from 'react-hook-form';
 import { Card, CardHeader, CardBody, Form, FormGroup, Button } from 'reactstrap';
-import { DIRECTOR_LIST, ADD_MOVIE } from '../query/query'
+import { MOVIE_LIST, DIRECTOR_LIST, ADD_MOVIE, ADD_DIRECTOR } from '../query/query'
 
 function SideNav() {
   const { data } = useQuery(DIRECTOR_LIST);
-  const [addMovie] = useMutation(ADD_MOVIE);
+  const [addMovie] = useMutation(ADD_MOVIE, { refetchQueries: [{ query: MOVIE_LIST }], awaitRefetchQueries: true });
+  const [addDirector] = useMutation(ADD_DIRECTOR, { refetchQueries: [{ query: MOVIE_LIST }], awaitRefetchQueries: true });
   const { handleSubmit, register, errors } = useForm();
   const onSubmit = ({ movieName, movieGenre, directorId }) => {
     addMovie({ variables: { name: movieName, genre: movieGenre, directorId } })
@@ -21,10 +22,10 @@ function SideNav() {
         <CardBody>
           <Form>
             <FormGroup>
-              <input className="form-control" type="text" name="directorName" pkaceholder="監督名" />
+              <input className="form-control" type="text" name="directorName" placeholder="監督名" />
             </FormGroup>
             <FormGroup>
-              <input className="form-control" type="number" name="directorAge" pkaceholder="年齢" />
+              <input className="form-control" type="number" name="directorAge" placeholder="年齢" />
             </FormGroup>
             <Button color="primary" type="submit">追加</Button>
           </Form>
